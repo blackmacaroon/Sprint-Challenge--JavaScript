335a772a759f00e3a2b5143a02a74fc2094ae6fa
Kayla <6lackmacaroon@gmail.com>
2019-04-05 10:03:06 -0600

    arrays uncomplete functions.js complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -110,7 +110,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+const animalNames = zooAnimals.forEach(element => {return [`${element[0]} ${element[2]}`]});
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -119,16 +119,16 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
-console.log(lowerCase); 
+// const lowerCase = zooAnimals.map(element => {return element.animal_name});
+// console.log(lowerCase.toLowerCase()); 
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
-console.log(largerPopulation);
+const largerPopulation = zooAnimals.filter(element => {return element.population < 5});
+console.log(largerPopulation );
 
 /* Request 4: .reduce() 
 
